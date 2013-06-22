afaa5eda89ae4f841b3043b9cc82d8b03bfe8bba
kaetemi <kaetemi@gmail.com>
2013-06-22 12:22:49 +0200

    Update app

--- app/app.cpp
+++ app/app.cpp
@@ -12,13 +12,15 @@
  */
 
 #include <SPI.h>
+#include <ft800emu_memory.h>
+#include <vc.h>
 
 void setup()
 {
-	
+	FT800EMU::Memory.rawWriteU32(FT800EMU::Memory.getRam(), REG_PCLK, 5);
 }
 
 void loop()
 {
-	
+	delay(10); // let's be nice on the cpu today :)
 }
