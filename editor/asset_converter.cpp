ba92152169d9411c2e4d63b418d1b49560ffe1de
kaetemi <kaetemi@gmail.com>
2014-02-10 00:22:23 +0100

    Display error message when python converter fails to load

--- editor/asset_converter.cpp
+++ editor/asset_converter.cpp
@@ -34,9 +34,11 @@ namespace {
 PyObject *a_ImageConvModule = NULL;
 PyObject *a_ImageConvObject = NULL;
 PyObject *a_ImageConvRun = NULL;
+QString a_ImageConvError;
 PyObject *a_RawConvModule = NULL;
 PyObject *a_RawConvObject = NULL;
 PyObject *a_RawConvRun = NULL;
+QString a_RawConvError;
 #endif /* FT800EMU_PYTHON */
 
 }
@@ -75,7 +77,11 @@ void AssetConverter::init()
 	if (error)
 	{
 		printf("---\nPython ERROR: \n");
-		PyErr_Print();
+		PyObject *ptype, *pvalue, *ptraceback;
+		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+		char *pStrErrorMessage = PyString_AsString(pvalue);
+		a_ImageConvError = QString::fromLocal8Bit(pStrErrorMessage);
+		printf("%s\n", pStrErrorMessage);
 		printf("---\n");
 	}
 
@@ -110,7 +116,11 @@ void AssetConverter::init()
 	if (error)
 	{
 		printf("---\nPython ERROR: \n");
-		PyErr_Print();
+		PyObject *ptype, *pvalue, *ptraceback;
+		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+		char *pStrErrorMessage = PyString_AsString(pvalue);
+		a_RawConvError = QString::fromLocal8Bit(pStrErrorMessage);
+		printf("%s\n", pStrErrorMessage);
 		printf("---\n");
 	}
 
@@ -134,47 +144,54 @@ void AssetConverter::release()
 
 void AssetConverter::convertImage(QString &buildError, const QString &inFile, const QString &outName, int format)
 {
-#ifdef FT800EMU_PYTHON
-	bool error = true;
-
-	QByteArray inFileU8 = inFile.toLocal8Bit(); // FIXME Unicode ?
-	QByteArray outNameU8 = outName.toLocal8Bit();
-	std::stringstream sFormat;
-	sFormat << format;
-
-	PyObject *pyValue;
-	PyObject *pyArgs = PyTuple_New(1);
-	PyObject *pyTuple = PyTuple_New(6);
-	pyValue = PyString_FromString("-i");
-	PyTuple_SetItem(pyTuple, 0, pyValue);
-	pyValue = PyString_FromString(inFileU8.data());
-	PyTuple_SetItem(pyTuple, 1, pyValue);
-	pyValue = PyString_FromString("-o");
-	PyTuple_SetItem(pyTuple, 2, pyValue);
-	pyValue = PyString_FromString(outNameU8.data());
-	PyTuple_SetItem(pyTuple, 3, pyValue);
-	pyValue = PyString_FromString("-f");
-	PyTuple_SetItem(pyTuple, 4, pyValue);
-	pyValue = PyString_FromString(sFormat.str().c_str());
-	PyTuple_SetItem(pyTuple, 5, pyValue);
-	PyTuple_SetItem(pyArgs, 0, pyTuple);
-	PyObject *pyResult = PyObject_CallObject(a_ImageConvRun, pyArgs);
-	Py_DECREF(pyArgs); pyArgs = NULL;
-	if (pyResult)
+#ifdef FT800EMU_PYTHON	
+	if (a_ImageConvRun)
 	{
-		printf("Image converted\n");
-		error = false;
+		bool error = true;
+
+		QByteArray inFileU8 = inFile.toLocal8Bit(); // FIXME Unicode ?
+		QByteArray outNameU8 = outName.toLocal8Bit();
+		std::stringstream sFormat;
+		sFormat << format;
+
+		PyObject *pyValue;
+		PyObject *pyArgs = PyTuple_New(1);
+		PyObject *pyTuple = PyTuple_New(6);
+		pyValue = PyString_FromString("-i");
+		PyTuple_SetItem(pyTuple, 0, pyValue);
+		pyValue = PyString_FromString(inFileU8.data());
+		PyTuple_SetItem(pyTuple, 1, pyValue);
+		pyValue = PyString_FromString("-o");
+		PyTuple_SetItem(pyTuple, 2, pyValue);
+		pyValue = PyString_FromString(outNameU8.data());
+		PyTuple_SetItem(pyTuple, 3, pyValue);
+		pyValue = PyString_FromString("-f");
+		PyTuple_SetItem(pyTuple, 4, pyValue);
+		pyValue = PyString_FromString(sFormat.str().c_str());
+		PyTuple_SetItem(pyTuple, 5, pyValue);
+		PyTuple_SetItem(pyArgs, 0, pyTuple);
+		PyObject *pyResult = PyObject_CallObject(a_ImageConvRun, pyArgs);
+		Py_DECREF(pyArgs); pyArgs = NULL;
+		if (pyResult)
+		{
+			printf("Image converted\n");
+			error = false;
+		}
+
+		if (error)
+		{
+			printf("---\nPython ERROR: \n");
+			PyObject *ptype, *pvalue, *ptraceback;
+			PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+			char *pStrErrorMessage = PyString_AsString(pvalue);
+			buildError = QString::fromLocal8Bit(pStrErrorMessage);
+			printf("%s\n", pStrErrorMessage);
+			printf("---\n");
+		}
 	}
-
-	if (error)
+	else
 	{
-		printf("---\nPython ERROR: \n");
-		PyObject *ptype, *pvalue, *ptraceback;
-		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
-		char *pStrErrorMessage = PyString_AsString(pvalue);
-		buildError = QString::fromLocal8Bit(pStrErrorMessage);
-		PyErr_Print();
-		printf("---\n");
+		buildError = "<i>(Python)</i> " + a_ImageConvError;
 	}
 #else
 	buildError = "Python not available";
@@ -183,53 +200,60 @@ void AssetConverter::convertImage(QString &buildError, const QString &inFile, co
 
 void AssetConverter::convertRaw(QString &buildError, const QString &inFile, const QString &outName, int begin, int length)
 {
-#ifdef FT800EMU_PYTHON
-	bool error = true;
-
-	QByteArray inFileU8 = inFile.toLocal8Bit(); // FIXME Unicode ?
-	QByteArray outNameU8 = outName.toLocal8Bit();
-	std::stringstream sBegin;
-	sBegin << begin;
-	std::stringstream sLength;
-	sLength << length;
-
-	PyObject *pyValue;
-	PyObject *pyArgs = PyTuple_New(1);
-	PyObject *pyTuple = PyTuple_New(8);
-	pyValue = PyString_FromString("-i");
-	PyTuple_SetItem(pyTuple, 0, pyValue);
-	pyValue = PyString_FromString(inFileU8.data());
-	PyTuple_SetItem(pyTuple, 1, pyValue);
-	pyValue = PyString_FromString("-o");
-	PyTuple_SetItem(pyTuple, 2, pyValue);
-	pyValue = PyString_FromString(outNameU8.data());
-	PyTuple_SetItem(pyTuple, 3, pyValue);
-	pyValue = PyString_FromString("-s");
-	PyTuple_SetItem(pyTuple, 4, pyValue);
-	pyValue = PyString_FromString(sBegin.str().c_str());
-	PyTuple_SetItem(pyTuple, 5, pyValue);
-	pyValue = PyString_FromString("-l");
-	PyTuple_SetItem(pyTuple, 6, pyValue);
-	pyValue = PyString_FromString(sLength.str().c_str());
-	PyTuple_SetItem(pyTuple, 7, pyValue);
-	PyTuple_SetItem(pyArgs, 0, pyTuple);
-	PyObject *pyResult = PyObject_CallObject(a_RawConvRun, pyArgs);
-	Py_DECREF(pyArgs); pyArgs = NULL;
-	if (pyResult)
+#ifdef FT800EMU_PYTHON	
+	if (a_RawConvRun)
 	{
-		printf("Raw converted\n");
-		error = false;
+		bool error = true;
+
+		QByteArray inFileU8 = inFile.toLocal8Bit(); // FIXME Unicode ?
+		QByteArray outNameU8 = outName.toLocal8Bit();
+		std::stringstream sBegin;
+		sBegin << begin;
+		std::stringstream sLength;
+		sLength << length;
+
+		PyObject *pyValue;
+		PyObject *pyArgs = PyTuple_New(1);
+		PyObject *pyTuple = PyTuple_New(8);
+		pyValue = PyString_FromString("-i");
+		PyTuple_SetItem(pyTuple, 0, pyValue);
+		pyValue = PyString_FromString(inFileU8.data());
+		PyTuple_SetItem(pyTuple, 1, pyValue);
+		pyValue = PyString_FromString("-o");
+		PyTuple_SetItem(pyTuple, 2, pyValue);
+		pyValue = PyString_FromString(outNameU8.data());
+		PyTuple_SetItem(pyTuple, 3, pyValue);
+		pyValue = PyString_FromString("-s");
+		PyTuple_SetItem(pyTuple, 4, pyValue);
+		pyValue = PyString_FromString(sBegin.str().c_str());
+		PyTuple_SetItem(pyTuple, 5, pyValue);
+		pyValue = PyString_FromString("-l");
+		PyTuple_SetItem(pyTuple, 6, pyValue);
+		pyValue = PyString_FromString(sLength.str().c_str());
+		PyTuple_SetItem(pyTuple, 7, pyValue);
+		PyTuple_SetItem(pyArgs, 0, pyTuple);
+		PyObject *pyResult = PyObject_CallObject(a_RawConvRun, pyArgs);
+		Py_DECREF(pyArgs); pyArgs = NULL;
+		if (pyResult)
+		{
+			printf("Raw converted\n");
+			error = false;
+		}
+
+		if (error)
+		{
+			printf("---\nPython ERROR: \n");
+			PyObject *ptype, *pvalue, *ptraceback;
+			PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+			char *pStrErrorMessage = PyString_AsString(pvalue);
+			buildError = QString::fromLocal8Bit(pStrErrorMessage);
+			printf("%s\n", pStrErrorMessage);
+			printf("---\n");
+		}
 	}
-
-	if (error)
+	else
 	{
-		printf("---\nPython ERROR: \n");
-		PyObject *ptype, *pvalue, *ptraceback;
-		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
-		char *pStrErrorMessage = PyString_AsString(pvalue);
-		buildError = QString::fromLocal8Bit(pStrErrorMessage);
-		PyErr_Print();
-		printf("---\n");
+		buildError = "<i>(Python)</i> " + a_RawConvError;
 	}
 #else
 	buildError = "Python not available";
