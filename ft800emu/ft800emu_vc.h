ebaea90dc742f124957f87a0aada376de10df635
kaetemi <kaetemi@gmail.com>
2015-02-12 23:28:26 +0100

    Include header

---
+++ ft800emu/ft800emu_vc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015  Future Technology Devices International Ltd
+ * Author: Jan Boon (jan.boon@kaetemi.be)
+ */
+
+#ifndef FT800EMU_VC_H
+#define FT800EMU_VC_H
+
+#ifdef FT810EMU_MODE
+#	include "vc2.h"
+#else
+#	include "vc.h"
+#endif
+
+#endif /* #ifndef FT800EMU_VC_H */
+
+/* end of file */
