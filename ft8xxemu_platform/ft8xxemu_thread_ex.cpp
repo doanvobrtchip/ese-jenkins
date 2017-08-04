0da45ec1fa8168184efe2025b9fcaa582c695311
kaetemi <kaetemi@gmail.com>
2017-08-04 19:43:50 +0800

    Simplified thread code

---
+++ ft8xxemu_platform/ft8xxemu_thread_ex.cpp
@@ -0,0 +1,117 @@
+/**
+ * ThreadEx
+ * \file ft8xxemu_thread_ex.cpp
+ * \brief ThreadEx
+ * \date 2017-08-04 10:57GMT
+ * \author Jan Boon (Kaetemi)
+ */
+
+/*
+ * Copyright (C) 2011-2017  Future Technology Devices International Ltd
+ */
+
+// #include <...>
+#include "ft8xxemu_thread_ex.h"
+
+// System includes
+#include "ft8xxemu_system_windows.h"
+
+// Project includes
+
+// using namespace ...;
+
+namespace FT8XXEMU {
+
+ThreadEx::ThreadEx()
+{
+	
+}
+
+ThreadEx::~ThreadEx()
+{
+	reset();
+}
+
+bool ThreadEx::init()
+{
+	reset();
+	if (DuplicateHandle(
+		GetCurrentProcess(),
+		GetCurrentThread(),
+		GetCurrentProcess(),
+		&m_Handle,
+		0,
+		TRUE,
+		DUPLICATE_SAME_ACCESS))
+	{
+		m_Id = GetCurrentThreadId();
+		return true;
+	}
+	reset();
+	return false;
+}
+
+bool ThreadEx::foreground()
+{
+	return !m_AvHandle && (m_AvHandle = AvSetMmThreadCharacteristicsA("Games", &m_AvTask));
+}
+
+void ThreadEx::reset()
+{
+	if (m_AvHandle)
+	{
+		AvRevertMmThreadCharacteristics(m_AvHandle);
+	}
+	m_AvTask = 0;
+	if (m_Handle)
+	{
+		CloseHandle(m_Handle);
+		m_Handle = NULL;
+	}
+	m_Id = 0;
+}
+
+
+bool ThreadEx::prioritize()
+{
+	return m_Handle && SetThreadPriority(m_Handle, THREAD_PRIORITY_HIGHEST);
+}
+
+bool ThreadEx::unprioritize()
+{
+	return m_Handle && SetThreadPriority(m_Handle, THREAD_PRIORITY_NORMAL);
+}
+
+bool ThreadEx::hold()
+{
+	return m_Handle && SuspendThread(m_Handle);
+}
+
+bool ThreadEx::resume()
+{
+	return m_Handle && ResumeThread(m_Handle);
+}
+
+bool ThreadEx::poke()
+{
+	if (m_Handle && SuspendThread(m_Handle))
+	{
+		while (!ResumeThread(m_Handle));
+		return true;
+	}
+	return false;
+}
+
+bool ThreadEx::kill()
+{
+	return m_Handle && TerminateThread(m_Handle);
+}
+
+bool ThreadEx::current()
+{
+	return GetCurrentThreadId() == m_Id;
+}
+
+} /* namespace FT8XXEMU */
+
+/* end of file */
---
+++ ft8xxemu_platform/ft8xxemu_thread_ex.h
@@ -0,0 +1,77 @@
+/**
+ * ThreadEx
+ * \file ft8xxemu_thread_ex.h
+ * \brief ThreadEx
+ * \date 2017-08-04 10:57GMT
+ * \author Jan Boon (Kaetemi)
+ */
+
+/*
+ * Copyright (C) 2011-2017  Future Technology Devices International Ltd
+ */
+
+#ifndef BT8XXEMU_THREAD_EX_H
+#define BT8XXEMU_THREAD_EX_H
+// #include <...>
+
+// System includes
+
+// Project includes
+
+namespace FT8XXEMU {
+
+/**
+ * ThreadEx
+ * \brief ThreadEx
+ * \date 2017-08-04 19:38GMT
+ * \author Jan Boon (Kaetemi)
+ */
+class ThreadEx
+{
+public:
+	ThreadEx();
+	~ThreadEx();
+
+	// Initialize for the current thread
+	bool init();
+
+	// Optimize the thread for foreground applications
+	bool foreground();
+
+	// Set the priority for this thread
+	bool prioritize();
+	bool unprioritize();
+
+	// Control this thread
+	bool hold();
+	bool resume();
+	bool poke();
+	bool kill();
+
+	// Is this the current thread
+	bool current();
+
+private:
+	void reset();
+
+private:
+#ifdef WIN32
+	HANDLE m_Handle = NULL;
+	DWORD m_Id = 0;
+	HANDLE m_AvHandle = NULL;
+	DWORD m_AvTask = 0;
+
+#else
+#endif
+	
+private:
+	ThreadEx(const ThreadEx &) = delete;
+	ThreadEx &operator=(const ThreadEx &) = delete;
+
+}; /* class ThreadEx */
+
+} /* namespace FT8XXEMU */
+
+#endif /* #ifndef BT8XXEMU_THREAD_EX_H */
+
+/* end of file */
