791aa0d779a9b1282ba8aff74a5218f9cf382195
Jan Boon <kaetemi@no-break.space>
2018-02-19 14:24:10 +0800

    Add additional flash sizes to editor

--- fteditor/constant_mapping_flash.cpp
+++ fteditor/constant_mapping_flash.cpp
@@ -15,19 +15,47 @@ namespace FTEDITOR {
 // TODO: Load from a simple CSV file
 
 const char *g_FlashToString[FTEDITOR_FLASH_NB] = { // Flash device names
+	"MX25L16 (2 MiB)",
+	"MX25L32 (4 MiB)",
 	"MX25L64 (8 MiB)",
+	"MX25L128 (16 MiB)",
+	"MX25L256 (32 MiB)",
+	"MX25L512 (64 MiB)",
+	"MX25L1024 (128 MiB)",
+	"MX25L2048 (256 MiB)",
 };
 
 const int g_FlashSizeBytes[FTEDITOR_FLASH_NB] = { // Flash sizes
+	2 * 1024 * 1024,
+	4 * 1024 * 1024,
 	8 * 1024 * 1024,
+	16 * 1024 * 1024,
+	32 * 1024 * 1024,
+	64 * 1024 * 1024,
+	128 * 1024 * 1024,
+	256 * 1024 * 1024,
 };
 
 const wchar_t *g_FlashDeviceType[FTEDITOR_FLASH_NB] = { // Library for flash device
+	L"mx25lemu",
+	L"mx25lemu",
+	L"mx25lemu",
+	L"mx25lemu",
+	L"mx25lemu",
+	L"mx25lemu",
+	L"mx25lemu",
 	L"mx25lemu",
 };
 
 const wchar_t *g_FlashFirmware[FTEDITOR_FLASH_NB] = { // Library for flash device
 	L"unified.blob",
+	L"unified.blob",
+	L"unified.blob",
+	L"unified.blob",
+	L"mx25l.blob",
+	L"mx25l.blob",
+	L"mx25l.blob",
+	L"mx25l.blob",
 };
 
 int g_CurrentFlash = FTEDITOR_DEFAULT_FLASH;
--- fteditor/constant_mapping_flash.h
+++ fteditor/constant_mapping_flash.h
@@ -15,7 +15,7 @@ namespace FTEDITOR {
 
 // ToString functions will return "" on invalid input. Simply check (result[0] == '\0') to find out if the result is invalid
 
-#define FTEDITOR_FLASH_NB 1
+#define FTEDITOR_FLASH_NB 8
 
 extern const char *g_FlashToString[FTEDITOR_FLASH_NB];
 extern const int g_FlashSizeBytes[FTEDITOR_FLASH_NB];
@@ -23,7 +23,7 @@ extern const wchar_t *g_FlashDeviceType[FTEDITOR_FLASH_NB];
 extern const wchar_t *g_FlashFirmware[FTEDITOR_FLASH_NB];
 
 extern int g_CurrentFlash;
-#define FTEDITOR_DEFAULT_FLASH 0
+#define FTEDITOR_DEFAULT_FLASH 2
 #define FTEDITOR_CURRENT_FLASH g_CurrentFlash
 
 inline const char *flashToString(int flashIntf) { return flashIntf < FTEDITOR_FLASH_NB ? g_FlashToString[flashIntf] : ""; }
