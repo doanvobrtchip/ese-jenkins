6019460e6d050335c64f3b48cb92dbea4e072642
Jan Boon <kaetemi@no-break.space>
2018-05-01 11:55:09 +0800

    Fix invalid delete call

--- fteditor/emulator_viewport.cpp
+++ fteditor/emulator_viewport.cpp
@@ -202,7 +202,7 @@ void EmulatorViewport::stop()
 		s_EmulatorThread = NULL;
 		printf("Emulator threads finished\n");
 
-        delete g_Emulator;
+		BT8XXEMU_destroy(g_Emulator);
 		g_Emulator = NULL;
 
 		if (g_Flash)
