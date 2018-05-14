0294807d188663bcc43de849bb2cbe5e545133ca
Jan Boon <kaetemi@no-break.space>
2018-05-14 14:23:02 +0800

    Include the right headers in deployment. Exclude 'btflash_defs.h', as it defines internal implementation specifics.

---
+++ deployment/emulator/include/bt8xxemu_flash.h
@@ -0,0 +1,66 @@
+/*
+BT8XX Emulator Library
+Copyright (C) 2017  Bridgetek Pte Lte
+Author: Jan Boon <jan@no-break.space>
+*/
+
+/*
+Header required only for implementing a flash emulation module for the BT8XX Emulator Library. Not documented
+*/
+
+#ifndef BT8XXEMU_FLASH_H
+#define BT8XXEMU_FLASH_H
+
+#include "bt8xxemu.h"
+
+namespace BT8XXEMU {
+	class Flash;
+}
+
+typedef struct 
+{
+	void(*Destroy)(BT8XXEMU::Flash *flash);
+
+	uint8_t(*TransferSpi4)(BT8XXEMU::Flash *flash, uint8_t signal);
+
+	uint8_t *(*Data)(BT8XXEMU::Flash *flash);
+	size_t(*Size)(BT8XXEMU::Flash *flash);
+
+} BT8XXEMU_FlashVTable;
+
+namespace BT8XXEMU {
+
+class Flash
+{
+public:
+	Flash(BT8XXEMU_FlashVTable *vTable) : m_VTable(vTable) { static_assert(offsetof(Flash, m_VTable) == 0, "Incompatible C++ ABI"); }
+
+	const BT8XXEMU_FlashVTable *vTable() const { return m_VTable; }
+
+protected:
+	BT8XXEMU_FlashVTable *m_VTable;
+
+};
+
+}
+
+#ifdef BT8XXEMU_FLASH_LIBRARY
+
+#ifdef WIN32
+#	define BT8XXEMU_EXPORT __declspec(dllexport)
+#else
+#	define BT8XXEMU_EXPORT
+#endif
+
+extern "C" 
+{
+
+BT8XXEMU_EXPORT extern BT8XXEMU_Flash *BT8XXEMU_Flash_create(uint32_t versionApi, const BT8XXEMU_FlashParameters *params);
+
+}
+
+#endif
+
+#endif /* #ifndef BT8XXEMU_FLASH_H */
+
+/* end of file */
