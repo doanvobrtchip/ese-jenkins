22fa402a1c04b16b98ef0c8c1af996d0d96f3b78
paul.jiao@ftdichip.com <paul.jiao@brtchip.com>
2018-05-24 14:47:08 +0800

    Update the REG_PLAY_CONTROL register to correct address and 8 bit read/write.

--- ft800emu/ft800emu_memory.cpp
+++ ft800emu/ft800emu_memory.cpp
@@ -560,8 +560,8 @@ Memory::Memory(FT8XXEMU::System *system, BT8XXEMU_EmulatorMode emulatorMode, std
 	rawWriteU32(REG_PLAYBACK_LENGTH, 0);
 	rawWriteU32(REG_PLAYBACK_START, 0);
 #if defined(BT815EMU_MODE)
-	rawWriteU32(REG_PLAYBACK_PAUSE, 0);
-	rawWriteU32(REG_PLAY_CONTROL, 1);
+	rawWriteU8(REG_PLAYBACK_PAUSE, 0);
+	rawWriteU8(REG_PLAY_CONTROL, 1);
 #endif
 
 	rawWriteU32(REG_TOUCH_TRANSFORM_A, 0x10000);
