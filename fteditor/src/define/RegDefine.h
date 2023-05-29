a11057ea6c668c2aad6730cf8f6ab0f925edbfb8
Liem Do <liem.do@brtchip.com>
2023-05-29 15:26:07 +0700

    #233: Saved REG_ROTATE into .ese + Defined value for some Registers

---
+++ fteditor/src/define/RegDefine.h
@@ -0,0 +1,19 @@
+/*!
+ * @file RegDefine.h
+ * @date 5/29/2023
+ * @author Liem Do <liem.do@brtchip.com>
+ */
+#ifndef REGDEFINE_H
+#define REGDEFINE_H
+
+// REG_ROTATE
+#define REG_ROTATE_MIN              0
+#define REG_ROTATE_MAX              7
+#define REG_ROTATE_DEFAULT          0
+
+// REG_PLAY_CONTROL
+#define REG_PLAY_CONTROL_EXIT       0xFF
+#define REG_PLAY_CONTROL_PAUSE      0x00
+#define REG_PLAY_CONTROL_PLAY       0x01
+
+#endif // REGDEFINE_H
